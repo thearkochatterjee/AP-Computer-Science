88f48105379c718c63148ece7a309ab2662a07c6
thearkochatterjee <arkochatterjee1006@gmail.com>
2017-11-29 11:14:23 -0500

    update

--- basic homework log/src/Gui.java
+++ basic homework log/src/Gui.java
@@ -2,6 +2,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 import java.util.ArrayList;
 
 public class Gui {
@@ -16,6 +17,12 @@ public class Gui {
     private static ArrayList<Assignment> arrassignments = new ArrayList<Assignment>();
     private static JList lsthw = new JList();
     private static DefaultListModel dlm = new DefaultListModel();
+    private static JTextField txtname = new JTextField();
+    private static JTextField txtsubject = new JTextField();
+    private static JTextField txtduedate = new JTextField();
+    private static JLabel lblname = new JLabel();
+    private static JLabel lblsubject = new JLabel();
+    private static JLabel lblduedate = new JLabel();
 
     public static void main(String args[]){
         gui.setTitle("Basic Homework Log");
@@ -33,7 +40,16 @@ public class Gui {
         mnuopen.setText("Open");
         mnuaddhomework.setText("Add Assignment");
         mnudelete.setText("Delete Assignment");
+        lblduedate.setText("Due Date");
+        lblname.setText("Name of Assignment");
+        lblsubject.setText("Subject");
         pane.add(lsthw);
+        pane.add(txtduedate);
+        pane.add(txtname);
+        pane.add(txtsubject);
+        pane.add(lblduedate);
+        pane.add(lblname);
+        pane.add(lblsubject);
         gui.setVisible(true);
         mnuaddhomework.addActionListener(new addassignment());
         mnudelete.addActionListener(new deleteassignment());
@@ -63,14 +79,35 @@ public class Gui {
     static class save implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            try(PrintWriter out = new PrintWriter("C:\\Users\\Arko\\Documents\\homework start up log\\homework.txt")){
+                for(int i = 0;i<arrassignments.size();i++){
+                    out.println(arrassignments.get(i).toString());
+                }
+                out.close();
+            } catch (FileNotFoundException e1) {
+                e1.printStackTrace();
+            }
         }
     }
 
     static class open implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            ArrayList<String> arrin = new ArrayList<String>();
+            try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Arko\\Documents\\homework start up log\\homework.txt"))){
+                String line;
+                while((line = br.readLine())!= null){
+                    arrin.add(line);
+                }
+                br.close();
+            } catch (FileNotFoundException e1){
+                e1.printStackTrace();
+            } catch (IOException e1){
+                e1.printStackTrace();
+            }
+            for(int i = 0;i<arrin.size();i++){
+                arrassignments.add(new Assignment(arrin.get(i)));
+            }
         }
     }
 }
