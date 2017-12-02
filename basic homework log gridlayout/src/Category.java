d08e06dc84b61b590c3c2c33845db898aacc9090
thearkochatterjee <arkochatterjee1006@gmail.com>
2017-12-02 00:55:51 -0500

    Update Category class

--- basic homework log gridlayout/src/Category.java
+++ basic homework log gridlayout/src/Category.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Category {
+    private String name;
+    private double weight;
+    private ArrayList<Assignments> arrassignment = new ArrayList<Assignments>();
+
+    public Category(){
+
+    }
 }
