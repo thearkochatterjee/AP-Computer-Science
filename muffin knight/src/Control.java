54e14b1bb8b4ee5faaa389504a14095dbec16029
thearkochatterjee <arkochatterjee1006@gmail.com>
2017-12-03 08:24:08 -0500

    Update muffin knight

--- muffin knight/src/Control.java
+++ muffin knight/src/Control.java
@@ -1,5 +1,7 @@
+
 public class Control {
     private static Maingui main = new Maingui();
+    private static Singleplayer single = new Singleplayer();
 
     public static void main(String args[]){
 
