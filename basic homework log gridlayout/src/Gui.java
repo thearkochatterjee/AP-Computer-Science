058819fe6cd7d0c26ad36fa12525defcefd64d6c
thearkochatterjee <arkochatterjee1006@gmail.com>
2017-12-03 12:25:28 -0500

    Update Homework log

--- basic homework log gridlayout/src/Gui.java
+++ basic homework log gridlayout/src/Gui.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
 import java.util.ArrayList;
 
 public class Gui {
@@ -26,10 +23,13 @@ public class Gui {
     private static JLabel lblearn = new JLabel();
     private static JMenuBar mnubar = new JMenuBar();
     private static JMenu mnufile = new JMenu();
+    private static JMenu mnusort = new JMenu();
     private static JMenuItem mnuadd = new JMenuItem();
     private static JMenuItem mnudelete = new JMenuItem();
     private static JMenuItem mnusave = new JMenuItem();
     private static JMenuItem mnuopen = new JMenuItem();
+    private static JMenuItem mnucompleted = new JMenuItem();
+    private static JMenuItem mnunotcompleted = new JMenuItem();
     private static JList lsthw = new JList();
     private static DefaultListModel dlm = new DefaultListModel();
     private static ArrayList<Assignments> arrassignments = new ArrayList<Assignments>();
@@ -43,6 +43,9 @@ public class Gui {
         gui.getContentPane().add(pane);
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mnubar.add(mnufile);
+        mnubar.add(mnusort);
+        mnusort.add(mnucompleted);
+        mnusort.add(mnunotcompleted);
         mnufile.add(mnuadd);
         mnufile.add(mnudelete);
         mnufile.add(mnusave);
@@ -91,11 +94,39 @@ public class Gui {
         mnudelete.setText("Delete Assignment");
         mnusave.setText("Save");
         mnuopen.setText("Open");
+        mnusort.setText("Organization");
+        mnucompleted.setText("Show Completed");
+        mnunotcompleted.setText("Show Not Completed");
+        openhomework("C:\\Users\\Arko\\Documents\\homework start up log\\homework.txt");
+        opensubject("C:\\Users\\Arko\\Documents\\homework start up log\\subject.txt");
+        opencategory("C:\\Users\\Arko\\Documents\\homework start up log\\category.txt");
         gui.setVisible(true);
+        gui.addKeyListener(new guiupdate());
         mnuadd.addActionListener(new add());
         mnudelete.addActionListener(new delete());
         mnusave.addActionListener(new save());
         mnuopen.addActionListener(new open());
+        mnunotcompleted.addActionListener(new showincomplete());
+        mnucompleted.addActionListener(new showcomplete());
+    }
+
+    static class guiupdate implements KeyListener{
+        @Override
+        public void keyTyped(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            if(e.getKeyCode()==KeyEvent.VK_ENTER){
+                update();
+            }
+        }
     }
 
     static class add implements ActionListener{
@@ -122,36 +153,96 @@ public class Gui {
     static class delete implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            dlm.remove(lsthw.getSelectedIndex());
+            arrassignments.remove(lsthw.getSelectedIndex());
         }
     }
 
     static class save implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            savehomework("C:\\Users\\Arko\\Documents\\homework start up log\\homework.txt");
+            savecategory("C:\\Users\\Arko\\Documents\\homework start up log\\category.txt");
+            savesubject("C:\\Users\\Arko\\Documents\\homework start up log\\subject.txt");
         }
     }
 
     static class open implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            openhomework("C:\\Users\\Arko\\Documents\\homework start up log\\homework.txt");
+            opensubject("C:\\Users\\Arko\\Documents\\homework start up log\\subject.txt");
+            opencategory("C:\\Users\\Arko\\Documents\\homework start up log\\category.txt");
         }
     }
 
     static class complete implements ItemListener{
         @Override
         public void itemStateChanged(ItemEvent e) {
-            arrassignments.get(lsthw.getSelectedIndex()).setComplete(true);
+            update();
         }
     }
 
-    public void open(String path){
+    static class showcomplete implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            dlm.clear();
+            for(int i = 0;i<arrassignments.size();i++){
+                if(arrassignments.get(i).isComplete()){
+                    dlm.addElement(arrassignments.get(i).getName());
+                }
+            }
+        }
+    }
+
+    static class showincomplete implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            dlm.clear();
+            for(int i = 0;i<arrassignments.size();i++){
+                if(arrassignments.get(i).isComplete()==false){
+                    dlm.addElement(arrassignments.get(i).getName());
+                }
+            }
+        }
+    }
+
+    public static void openhomework(String path){
 
     }
 
-    public void save(String path){
+    public static void opensubject(String path){
 
+    }
+
+    public static void opencategory(String path){
+
+    }
+
+    public static void savehomework(String path){
+
+    }
+
+    public static void savesubject(String path){
+
+    }
+
+    public static void savecategory(String path){
+
+    }
+
+    public static void update(){
+        arrassignments.get(lsthw.getSelectedIndex()).setName(txtname.getText());
+        arrassignments.get(lsthw.getSelectedIndex()).setSubject(txtsubject.getText());
+        arrassignments.get(lsthw.getSelectedIndex()).setCategory(txtcategory.getText());
+        try{
+            arrassignments.get(lsthw.getSelectedIndex()).setEarn(Double.valueOf(txtearn.getText()));
+            arrassignments.get(lsthw.getSelectedIndex()).setTotal(Double.valueOf(txttotal.getText()));
+        }
+        catch (Exception e){
+            JOptionPane.showMessageDialog(null,"Input Error");
+        }
+        arrassignments.get(lsthw.getSelectedIndex()).setDue(new DueDate(txtduedate.getText()));
+        arrassignments.get(lsthw.getSelectedIndex()).setComplete(chxcomplete.isSelected());
     }
 }
