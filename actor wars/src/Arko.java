764d2668563b5d41c4aa09f61905e36d448f89b2
thearkochatterjee <arkochatterjee1006@gmail.com>
2018-04-16 09:46:23 -0400

    Adding Intial Actor Wars

---
+++ actor wars/src/Arko.java
@@ -0,0 +1,14 @@
+import org.masonacm.actorwars.HolyHandGrenadeBearer;
+import org.masonacm.actorwars.Peon;
+import org.masonacm.actorwars.Wheat;
+
+public class Arko extends Peon{
+    public Arko(){
+
+    }
+
+    @Override
+    public void peonAct() {
+
+    }
+}
---
+++ actor wars/src/Tester.java
@@ -0,0 +1,13 @@
+import info.gridworld.grid.Location;
+import org.masonacm.actorwars.ActorWarWorld;
+import org.masonacm.actorwars.HolyHandGrenadeBearer;
+import org.masonacm.actorwars.Iron;
+import org.masonacm.actorwars.Peon;
+
+public class Tester {
+    public static void main(String args[]){
+        ActorWarWorld world = new ActorWarWorld();
+
+        world.show();
+    }
+}
