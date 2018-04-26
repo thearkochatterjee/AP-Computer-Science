647257db51683f082b3dd7ba56aef1648a1f6c1e
thearkochatterjee <arkochatterjee1006@gmail.com>
2018-04-26 12:33:10 -0400

    Update

--- actor wars/src/Arko.java
+++ actor wars/src/Arko.java
@@ -5,7 +5,9 @@ import com.sun.nio.sctp.PeerAddressChangeNotification;
 import info.gridworld.actor.Actor;
 import info.gridworld.grid.Location;
 import org.masonacm.actorwars.*;
+import org.masonacm.actorwars.Action;
 
+import javax.swing.*;
 import java.util.ArrayList;
 
 public class Arko extends Peon{
@@ -18,22 +20,85 @@ public class Arko extends Peon{
 
     @Override
     public void peonAct() {
+        int i = 0;
         try{
-            wheathunting();
+            if(i == 0) {
+                hunting(Wheat.class);
+            }
+            else if(i==1){
+                hunting(Peon.class);
+            }
         }
         catch (Exception e){
-            playerhunting();
+            i++;
+
         }
     }
 
-    private void playerhunting(){
-
+    private void hunting(Class<?> e){
+        ModifiableLocation m = new ModifiableLocation();
+        boolean fix = false;
+//        if((arrpath.size() < 3) && (arrpath.size() > 1) && !fix){
+//            arrpath = Pathfinder.findPath(getLocation(), searchfor(2,e), getGrid());
+//            fix = true;
+//        }
+//        else if(arrpath.size() < 1){
+//            arrpath = Pathfinder.findPath(getLocation(), LocationFinder.findClosestInstanceLocation(getLocation(), e, getGrid()), getGrid());
+//        }
+        if(arrpath.size() < 1){
+            arrpath = Pathfinder.findPath(getLocation(), LocationFinder.findClosestInstanceLocation(getLocation(), e, getGrid()), getGrid());
+        }
+        else {
+            m.setValue(getLocation());
+            setDirection(m.directionTo(arrpath.get(0)));
+            move();
+            arrpath.remove(0);
+        }
+        if(close(e)){
+            setDirection(m.directionTo(LocationFinder.findClosestInstanceLocation(getLocation(), e,getGrid())));
+            myactions.clear();
+            myactions.add(Action.attackHP(5));
+        }
         survive();
     }
 
-    private void ironhunting(){
+    public void showpath(){
+        for(Location l: arrpath){
+            JOptionPane.showMessageDialog(null,l.toString());
+        }
+    }
 
-        survive();
+    public Location searchfor(int radius, Class<?> e){
+        int startr = getLocation().getRow()-radius;
+        int startc = getLocation().getCol()-radius;
+        ModifiableLocation m = new ModifiableLocation();
+        for(int r = startr; r<(startr + 2 * radius); r++){
+            for(int c = startc; c<(startc + 2 * radius); c++){
+                m.setValue(getLocation());
+                for(Actor a: getGrid().getNeighbors(new Location(r,c))){
+                    if(e.isInstance(a)){
+                        return a.getLocation();
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public void adjust(){
+        ModifiableLocation m = new ModifiableLocation();
+        if(arrpath.size()==2){
+            for(int i = 0;i<2;i++){
+                m.setValue(getLocation());
+                for(Actor a: getGrid().getNeighbors(arrpath.get(i))){
+                    if(a instanceof Wheat){
+                        setDirection(m.directionTo(arrpath.get(i)));
+                        myactions.clear();
+                        myactions.add(Action.attackHP(5));
+                    }
+                }
+            }
+        }
     }
 
     private void wheathunting(){
@@ -70,6 +135,15 @@ public class Arko extends Peon{
     public boolean hasWheat(){
         for(Location l: getGrid().getOccupiedLocations()){
             if(getGrid().get(l) instanceof Wheat){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean close(Class<?> e){
+        for(Actor a: getGrid().getNeighbors(getLocation())){
+            if(e.isInstance(a)){
                 return true;
             }
         }
--- actor wars/src/Tester.java
+++ actor wars/src/Tester.java
@@ -1,15 +1,20 @@
 import info.gridworld.actor.Actor;
 import info.gridworld.grid.Location;
-import org.masonacm.actorwars.ActorWarWorld;
-import org.masonacm.actorwars.HolyHandGrenadeBearer;
-import org.masonacm.actorwars.Iron;
-import org.masonacm.actorwars.Peon;
+import org.masonacm.actorwars.*;
 
 public class Tester {
     public static void main(String args[]){
         ActorWarWorld world = new ActorWarWorld();
         world.add(new Arko());
-        world.add(new Arko());
+        world.add(new Peon() {
+            @Override
+            public void peonAct() {
+
+            }
+        });
+//        for(int i = 0;i<50;i++){
+//            world.add(new Wheat());
+//        }
         world.show();
     }
 }
